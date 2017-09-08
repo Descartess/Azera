6204949f55fe049872b26f01a15b2ef87290ed0e
Descartess <nyondopaul23@gmail.com>
2017-09-08 23:26:06 +0300

    Add layout

--- src/containers/app.jsx
+++ src/containers/app.jsx
@@ -1,16 +1,21 @@
 import React, { Component } from 'react';
+import { Grid } from 'semantic-ui-react';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
   render() {
     return (
-      <div className="App">
-        <div className="App-header">
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+      <Grid>
+        <Grid.Column width={2}>
+          <p>Left Panel</p>
+        </Grid.Column>
+        <Grid.Column width={14}>
+          <p>Right Panel</p>
+        </Grid.Column>
+      </Grid>
     );
   }
 }
