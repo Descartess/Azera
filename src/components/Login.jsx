21c372f7ead007fdeaf0f33425db1ec4c4dc97d4
Sebuuma Daniel <sedzsoft@gmail.com>
2017-09-09 09:11:20 +0300

    Updates the look of the login page

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -4,7 +4,14 @@ import { Button } from 'semantic-ui-react';
 class Login extends Component {
     render() {
         return (
-            <Button onClick={this.props.handleGoogleLogin}>Login/SignIn</Button>
+            <div className="login dark-primary-color">
+                <span className="logo2">
+                    AZERA
+                </span>
+                <Button basic color='teal' size='massive' onClick={this.props.handleGoogleLogin}>
+                    Login/SignIn
+                </Button>
+            </div>
         );
     }
 }
