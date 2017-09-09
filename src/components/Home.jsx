65c66e31f94d2bf04ac258c917bad5c3ce6933e5
SProjects <sedzsoft@gmail.com>
2017-09-09 04:45:12 +0300

    Updates Home landing page

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,10 +1,38 @@
 import React, { Component } from 'react';
+import { Grid } from 'semantic-ui-react';
+import Stats from '../components/stats';
+import HeaderBar from '../components/header';
+import ReceiptCard from '../components/card';
 import { Button } from 'semantic-ui-react';
 
 class Home extends Component {
     render() {
         return (
+            <Grid className="full-height">
+                <Grid.Column width={3} className="column1">
+                  <Grid.Row>
+                    <div className="logo">
+                      <p>AZERA</p>
+                    </div>
+                  </Grid.Row>
+                  <div className="left-bar">
+                    <Stats />
+                    <Stats />
+                    <Stats />
+                  </div>
+                </Grid.Column >
+                <Grid.Column width={13} className="column2">
+                  <HeaderBar />
+                  <Grid.Row>
+                    <div className="main-body">
+                      <ReceiptCard />
+                      <Button onClick={this.props.handleLogout}>Logout</Button>
+                    </div>
+                  </Grid.Row>
+                </Grid.Column>
+              </Grid>
             <div>
+                
                 <h2>You have logged in successfully</h2>
                 <Button onClick={this.props.handleLogout}>Logout</Button>
             </div>
