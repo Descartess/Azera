2a5bd71108ef30d7bcb229baf5180f84ec66815a
Descartess <nyondopaul23@gmail.com>
2017-09-09 04:31:39 +0300

    Add static design

---
+++ src/components/stats.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { Statistic } from 'semantic-ui-react';
+
+const Stats = () => (
+  <div className="stats">
+    <Statistic value="500" label="Receipts" />
+  </div>
+);
+
+export default Stats;
--- src/containers/app.jsx
+++ src/containers/app.jsx
@@ -1,5 +1,8 @@
 import React, { Component } from 'react';
 import { Grid } from 'semantic-ui-react';
+import Stats from '../components/stats';
+import HeaderBar from '../components/header';
+import ReceiptCard from '../components/card';
 
 class App extends Component {
   constructor(props) {
@@ -8,12 +11,26 @@ class App extends Component {
   }
   render() {
     return (
-      <Grid>
-        <Grid.Column width={2}>
-          <p>Left Panel</p>
-        </Grid.Column>
-        <Grid.Column width={14}>
-          <p>Right Panel</p>
+      <Grid className="full-height">
+        <Grid.Column width={3} className="column1">
+          <Grid.Row>
+            <div className="logo">
+              <p>AZERA</p>
+            </div>
+          </Grid.Row>
+          <div className="left-bar">
+            <Stats />
+            <Stats />
+            <Stats />
+          </div>
+        </Grid.Column >
+        <Grid.Column width={13} className="column2">
+          <HeaderBar />
+          <Grid.Row>
+            <div className="main-body">
+              <ReceiptCard />
+            </div>
+          </Grid.Row>
         </Grid.Column>
       </Grid>
     );
