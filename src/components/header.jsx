798e099dab99e8c13a3b9eda2eed12ab597cf99f
Descartess <nyondopaul23@gmail.com>
2017-09-09 10:06:27 +0300

    Improve UI designs

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,8 +1,12 @@
 import React from 'react';
 
 const Header = () => (
-  <div className="logo">
-    <p> Azera </p>
+  <div className="logo2">
+    <div className="ui horizontal small divided list">
+      <a className="item">Paul </a>
+      <a className="item"> Account Settings</a>
+      <a className="item"> Logout</a>
+    </div>
   </div>
 );
 
