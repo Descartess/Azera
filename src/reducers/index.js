f8b8bf001cbe07fd4ca46cf751f0691497318821
Descartess <nyondopaul23@gmail.com>
2017-09-10 16:17:41 +0300

    Add receipts Reducers

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,8 +1,10 @@
 import { combineReducers } from 'redux';
 import AuthReducer from './AuthReducer';
+import ReceiptsReducer from './receiptsReducer';
 
 const reducers = combineReducers({
   auth: AuthReducer,
+  receipts: ReceiptsReducer,
 });
 
 export default reducers;
