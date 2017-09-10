ee9c2231cbfc39529cab774755bed5642a16561f
Descartess <nyondopaul23@gmail.com>
2017-09-10 20:38:46 +0300

    Convert timestamp to Date String

--- src/components/card.jsx
+++ src/components/card.jsx
@@ -9,7 +9,7 @@ const ReceiptCard = (props) => (
         <Icon className="right floated check green circular icon" title="Approve"/>
         <Icon className="right floated eye circular icon" title="Approve"/>
       <Card.Header>{props.catergory}</Card.Header>
-      <Card.Meta>{props.createdAt}</Card.Meta>
+      <Card.Meta>{new Date(props.createdAt).toDateString()}</Card.Meta>
       <Card.Description>Daniel is a comedian living in Nashville.</Card.Description>
     </Card.Content>
     <Card.Content extra>
